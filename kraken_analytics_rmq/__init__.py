9edfa501deec8680b14532987d121ad0ed90454e
Thomas Rommel <thomasrommel89@gmail.com>
2023-09-11 12:55:51 +0200

    General refactoring

--- kraken_analytics_rmq/__init__.py
+++ kraken_analytics_rmq/__init__.py
@@ -1,11 +1,18 @@
 import json
-from typing import Any, Dict
+from typing import Any, Callable, Dict, List
 
 import pika
 
 
 def handle_errors(func):
+    """
+    Decorator for handling errors when consuming messages
+    """
+
     def wrapper(self, ch, method, properties, body):
+        """
+        Wrapper function for handling errors when consuming messages
+        """
         try:
             # Check the 'x-death' headers, which are automatically assigned when a message is
             # transferred to the retry queue, expires, and is then returned to its original queue.
@@ -15,7 +22,7 @@ def handle_errors(func):
                         self.settings["retry_count"]
                     ):
                         # send message to dead letter queue
-                        self.send_direct_message(
+                        self.__send_direct_message(
                             message=json.loads(body),
                             routing_key=self.settings["queue_dead_letter"],
                         )
@@ -24,7 +31,7 @@ def handle_errors(func):
             func(self, ch, method, properties, body)
 
         except:
-            self.send_message(
+            self.__send_message(
                 message=json.loads(body),
                 headers=properties.headers,
                 routing_key=self.settings["queue_retry"],
@@ -36,7 +43,7 @@ def handle_errors(func):
 class RMQ:
     def __init__(
         self,
-        business_logic_function,
+        business_logic_function: Callable,
         settings: Dict,
         connection=None,
         channel=None,
@@ -46,9 +53,12 @@ class RMQ:
         self.connection = connection
         self.channel = channel
         if self.connection is None or self.channel is None:
-            self._setup_connection_and_channel()
+            self.__setup_connection_and_channel()
 
-    def _setup_connection_and_channel(self):
+    def __setup_connection_and_channel(self):
+        """
+        Setup connection and channel for consuming messages
+        """
         credentials = pika.PlainCredentials(
             self.settings["user"], self.settings["password"]
         )
@@ -62,7 +72,7 @@ class RMQ:
         )
         self.channel = self.connection.channel()
 
-    def send_message(
+    def __send_message(
         self,
         message: Dict[str, Any],
         exchange: str = None,
@@ -72,7 +82,7 @@ class RMQ:
         """
         Send message via exchange to a queue
         """
-        headers = headers or {}
+        headers: Dict = headers or {}
 
         self.channel.basic_publish(
             exchange=exchange,
@@ -82,25 +92,34 @@ class RMQ:
         )
 
     def consume_messages(self):
+        """
+        Consume messages from a queue and pass them to the callback function
+        """
         self.channel.basic_consume(
             queue=self.settings["queue_consume"],
-            on_message_callback=self.callback,
+            on_message_callback=self.__callback,
             auto_ack=True,
         )
         self.channel.start_consuming()
 
-    def send_direct_message(self, message: Dict[str, Any], routing_key: str):
-        self.send_message(message=message, exchange="", routing_key=routing_key)
+    def __send_direct_message(self, message: Dict[str, Any], routing_key: str):
+        """
+        Send a message directly to a queue without an exchange
+        """
+        self.__send_message(message=message, exchange="", routing_key=routing_key)
 
     @handle_errors
-    def callback(self, ch, method, properties, body):
-        message_body = json.loads(body)
+    def __callback(self, ch, method, properties, body):
+        """
+        Callback function for consuming messages
+        """
+        message_body: Dict = json.loads(body)
 
-        response = self.business_logic_function(message_body=message_body)
+        response: List[Dict] = self.business_logic_function(message_body=message_body)
         for item in response:
             message: Dict[str, Any] = item["message"]
             routing_key: str = item["routing_key"]
-            self.send_message(
+            self.__send_message(
                 message=message,
                 exchange=self.settings["exchange"],
                 routing_key=routing_key,
